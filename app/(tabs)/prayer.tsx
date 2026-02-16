ac646ca2f63c67b4fc7c832fd434a93f5f40cc89
Aqsa <aqsaabdullah38403@gmail.com>
2026-02-16 06:34:12 +0500

    add tabs icons

--- app/(tabs)/prayer.tsx
+++ app/(tabs)/prayer.tsx
@@ -1,126 +1,111 @@
-import { Image } from "expo-image";
-import { Platform, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 
-import { ExternalLink } from "@/components/external-link";
-import ParallaxScrollView from "@/components/parallax-scroll-view";
-import { ThemedText } from "@/components/themed-text";
-import { ThemedView } from "@/components/themed-view";
-import { Collapsible } from "@/components/ui/collapsible";
-import { IconSymbol } from "@/components/ui/icon-symbol";
-import { Fonts } from "@/constants/theme";
-
-export default function TabTwoScreen() {
+export default function PrayerScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
+    <View className="flex-1 bg-white text-black pt-12">
+      {/* --- Header Section --- */}
+
+      <View className="p-5 items-center">
+        <Text className=" text-[#666] text-[12px] mb-8">
+          QPGQ+WJC, Wah, Punjab, Pakistan
+        </Text>
+        <View className="flex-row items-center justify-between w-full mb-4">
+          {/* Sunrise */}
+          <View className="items-center">
+            <Text className=" text-xs mb-1">Sunrise</Text>
+            <Ionicons name="sunny-outline" size={24} color="#000" />
+            <Text className="text-[13px] mt-1">06:53 am</Text>
+          </View>
+
+          {/* Center Info */}
+          <View className="items-center flex-1">
+            <Text className="font-bold my-1">SHAFI</Text>
+            <Text className=" my-[2px]">Now: Fajir time</Text>
+            <Text className="font-bold">(Started from 15:27 AM)</Text>
+
+            <View className="w-[60%] border-dashed border-[0.5px] border-[#555] my-1" />
+            <Text className=" my-[2px]">Next: Sunrise time</Text>
+            <Text className="font-bold">(Starts to 15:27 AM)</Text>
+          </View>
+
+          {/* Sunset */}
+          <View className="items-center">
+            <Text className="text-xs mb-1">Sunset</Text>
+            <Ionicons name="sunny" size={24} color="#000" />
+            <Text className="t text-[13px] mt-1">05:54 pm</Text>
+          </View>
+        </View>
+      </View>
+
+      {/* --- Date Selector --- */}
+      <View className="flex-row justify-between items-center bg-[#252f39] mx-4 my-4 p-3 rounded-xl">
+        <TouchableOpacity>
+          <Ionicons name="chevron-back" size={24} color="#fff" />
+        </TouchableOpacity>
+
+        <View className="items-center">
+          <Text className="text-white font-bold">16 Feb, 2026</Text>
+          <Text className="text-[#aaa] text-xs">28 Sha'aban 1447</Text>
+        </View>
+
+        <TouchableOpacity>
+          <Ionicons name="chevron-forward" size={24} color="#fff" />
+        </TouchableOpacity>
+      </View>
+
+      {/* --- Prayer List --- */}
+      <ScrollView className="px-4">
+        <PrayerRow
+          name="Fajr"
+          time="5:22 AM - 6:52 AM"
+          icon="cloudy-night-outline"
         />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText
-          type="title"
-          style={{
-            fontFamily: Fonts.rounded,
-          }}
-        >
-          Prayer
-        </ThemedText>
-      </ThemedView>
-      <ThemedText>
-        This app includes example code to help you get started.
-      </ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          and{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{" "}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the
-          web version, press <ThemedText type="defaultSemiBold">w</ThemedText>{" "}
-          in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the{" "}
-          <ThemedText type="defaultSemiBold">@2x</ThemedText> and{" "}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to
-          provide files for different screen densities
-        </ThemedText>
-        <Image
-          source={require("@/assets/images/react-logo.png")}
-          style={{ width: 100, height: 100, alignSelf: "center" }}
+        <PrayerRow
+          name="Sunrise"
+          time="6:53 AM"
+          icon="sunny-outline"
+          highlight
         />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{" "}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook
-          lets you inspect what the user&apos;s current color scheme is, and so
-          you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{" "}
-          <ThemedText type="defaultSemiBold">
-            components/HelloWave.tsx
-          </ThemedText>{" "}
-          component uses the powerful{" "}
-          <ThemedText type="defaultSemiBold" style={{ fontFamily: Fonts.mono }}>
-            react-native-reanimated
-          </ThemedText>{" "}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The{" "}
-              <ThemedText type="defaultSemiBold">
-                components/ParallaxScrollView.tsx
-              </ThemedText>{" "}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+        <PrayerRow name="Dhuhr" time="12:24 PM - 3:29 PM" icon="sunny" />
+        <PrayerRow
+          name="Asr (Shafi)"
+          time="3:30 PM - 5:39 PM"
+          icon="cloud-outline"
+        />
+        <PrayerRow
+          name="Maghrib"
+          time="5:40 PM - 5:54 PM"
+          icon="sunny-outline"
+        />
+        <PrayerRow name="sunset" time="5:54 PM" icon="sunny-outline" />
+        <PrayerRow name="Isha" time="5:55 PM - 6:20 PM" icon="moon-outline" />
+      </ScrollView>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  headerImage: {
-    color: "#808080",
-    bottom: -90,
-    left: -35,
-    position: "absolute",
-  },
-  titleContainer: {
-    flexDirection: "row",
-    gap: 8,
-  },
-});
+// Reusable Row Component
+const PrayerRow = ({ name, time, icon, highlight }: any) => (
+  <View
+    className={`flex-row justify-between items-center py-4 border-b border-[#222] ${
+      highlight ? "bg-[#1A2344]  rounded-lg px-3" : ""
+    }`}
+  >
+    <View className="flex-row items-center">
+      <Ionicons name={icon} size={22} color={highlight ? "#fff" : "#aaa"} />
+      <Text
+        className={`ml-4 text-[15px] ${
+          highlight ? "text-white" : "text-black"
+        }`}
+      >
+        {name}
+      </Text>
+    </View>
+
+    <Text className={`text-sm ${highlight ? "text-black" : "text-[#ccc]"}`}>
+      {time}
+    </Text>
+  </View>
+);
